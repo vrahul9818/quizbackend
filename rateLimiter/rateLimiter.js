400dda05effda348b43323abd4bcd47f656dea1a
vrahul9818 <122368342+vrahul9818@users.noreply.github.com>
2023-05-28 22:36:37 +0530

    rate limit

--- rateLimiter/rateLimiter.js
+++ rateLimiter/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20, // Max number of requests 
+  windowMs: 15 * 60 * 1000, 
+  max: 2000, // Max number of requests 
   message: 'Too many requests ',
 });
 
